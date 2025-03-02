41fb56239699bcafeb4cddab8b67a6e23d2e7337
hemaxpatel <hemaxpatel3124@gmail.com>
2025-03-02 08:09:02 +0530

    Add All Transactions component and update navigation; fix transaction amount display

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,6 +14,7 @@ import LoanAgreement from './pages/LoanAgreement';
 import TransactionHistory from './components/TransactionHistory';
 import GroupDetail from './components/GroupDetail';
 import CreateLoan from './pages/CreateLoan';
+import AllTransactions from './components/showAllTransaction';
 
 const router = createBrowserRouter([
   {
@@ -39,6 +40,10 @@ const router = createBrowserRouter([
           {
             path: 'profile',
             element: <ProfilePage />,
+          },
+          {
+            path: 'all-reports',
+            element: <AllTransactions />,
           },
         ],
       },
